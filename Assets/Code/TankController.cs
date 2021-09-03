cf07385123b4cc65ab109e94bed478fe0caabde1
Aryan Kapoor <aryankap@usc.edu>
2021-09-02 17:59:36 -0700

    Initial commit

---
+++ Assets/Code/TankController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class TankController : NetworkBehaviour {
+	public float MoveSpeed = 150.0f;
+	public float RotateSpeed = 3.0f;
+	public Color LocalPlayerColor;
+	public GameObject ShotPrefab;
+	public Transform ShotSpawnTransform;
+	public float ShotSpeed;
+	public float ReloadRate = 0.5f;
+	private float _nextShotTime;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public override void OnStartLocalPlayer(){
+		var tankParts = GetComponentsInChildren<MeshRenderer> ();
+		foreach (var part in tankParts) {
+			part.material.color = LocalPlayerColor;
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer) {
+			return;
+		}
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * MoveSpeed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * RotateSpeed;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+
+		if (Input.GetKeyDown (KeyCode.Space) && Time.time > _nextShotTime) {
+			CmdFire ();
+		}
+	}
+
+	[Command]
+	void CmdFire(){
+		_nextShotTime = Time.time + ReloadRate;
+		var bullet = Instantiate (ShotPrefab, ShotSpawnTransform.position, Quaternion.identity) as GameObject;
+		bullet.GetComponent<Rigidbody> ().velocity = transform.forward * ShotSpeed;
+
+		NetworkServer.Spawn (bullet);
+	}
+
+	void OnCollisionEnter(Collision collision){
+		var other = collision.gameObject;
+		try{
+			var causeDamageScript = other.GetComponent<CauseDamage>();
+			var totalDamage = causeDamageScript.GetDamage();
+			var healthScript = GetComponent<Health>();
+			healthScript.TakeDamage(totalDamage);
+			Debug.Log("Shot");
+		} catch {
+			Debug.Log ("Something hit us but didn't do any damage.");
+		}
+	}
+}
---
+++ Assets/Health.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+	public int maxHealth;
+
+	[SyncVar(hook = "OnHealthChanged")]
+	public int currentHealth;
+	public Text HealthScore;
+
+	// Use this for initialization
+	void Start () {
+		HealthScore.text = currentHealth.ToString();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TakeDamage(int howMuch){
+
+		if (!isServer)
+		{
+			return;
+		}
+			var newHealth = currentHealth - howMuch;
+			if (newHealth <= 0)
+			{
+			currentHealth = maxHealth;
+			//Debug.Log("Dead");
+			RpcDeath();
+			}
+			else
+			{
+				currentHealth = newHealth;
+			//HealthScore.text = currentHealth.ToString();
+		}
+		//}
+	}
+
+	void OnHealthChanged(int updatedHealth)
+    {
+		HealthScore.text = updatedHealth.ToString();
+	}
+
+	[ClientRpc]
+	void RpcDeath()
+    {
+		if(isLocalPlayer)
+        {
+			//transform.position = Vector3.zero;
+			var spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+			var chosenPoint = Random.Range(0, spawnPoints.Length);
+			transform.position = spawnPoints[chosenPoint].transform.position;
+
+        }
+    }
+}
